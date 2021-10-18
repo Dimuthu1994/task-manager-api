969ad5674e8a669f20c88e6fa42a7cf9e6ba2d30
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-10-18 08:58:53 +0530

    filtering data

--- src/routers/task.js
+++ src/routers/task.js
@@ -16,15 +16,17 @@ router.post("/tasks", auth, async (req, res) => {
 
 // GET /tasks?completed=false
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
   try {
     //below also works
     //const result = await Task.find({owner:req.user._id});
 
     await req.user.populate({
       path: "tasks",
-      match: {
-        completed: false,
-      },
+      match,
     });
     res.send(req.user.tasks);
   } catch (e) {
