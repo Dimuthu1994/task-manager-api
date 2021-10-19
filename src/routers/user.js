fb7b485e01a2177c57f7c427a0a3c1bafd309d8f
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-10-19 08:49:31 +0530

    handling file upload errors

--- src/routers/user.js
+++ src/routers/user.js
@@ -122,7 +122,7 @@ const upload = multer({
     fileSize: 1000000,
   },
   fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg)$/)) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
       return cb(new Error("Please upload an image"));
     }
 
@@ -130,8 +130,15 @@ const upload = multer({
   },
 });
 
-router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
-  res.send();
-});
+router.post(
+  "/users/me/avatar",
+  upload.single("avatar"),
+  (req, res) => {
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 module.exports = router;
